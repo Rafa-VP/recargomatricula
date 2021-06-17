66b48666f780f7d648ac4cceaec2fa13f9f3445f
Rafael <76231028+rafaelvp17@users.noreply.github.com>
2021-06-17 12:35:21 -0500

    inicio

--- recargomatricula.cpp
+++ recargomatricula.cpp
@@ -5,8 +5,7 @@ main(){
 /* Se ingresa la edad de un estudiante. si la edad es de 5 a 10 años, se hace un recargo
 del 1% al costo de la matrícula. Si la edad es superior a 10 años y menor o igual que 15 años,
 el recargo será del 2%. Si la edad es mayor que 15 y menor o igual que 20, el recargo es del
-4%. Si la edad es superior a 20 años, el recargo es del 6% */
-
-
+4%. Si la edad es superior a 20 años, el recargo es del 6%.
+Para las edades inferiores a 5 años, no hay recargos */
 
 }
