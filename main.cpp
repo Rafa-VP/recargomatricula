4cf1d9d7034e73630ad09c4ba95b4fab9dcabddf
Rafael <76231028+rafaelvp17@users.noreply.github.com>
2021-06-17 12:00:58 -0500

    inicio

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main(){
+
+/* Se ingresa la edad de un estudiante. si la edad es de 5 a 10 años, se hace un recargo
+del 1% al costo de la matrícula. Si la edad es superior a 10 años y menor o igual que 15 años,
+el recargo será del 2%. Si la edad es mayor que 15 y menor o igual que 20, el recargo es del
+4%. Si la edad es superior a 20 años, el recargo es del 6% */
+
+
+
+}
